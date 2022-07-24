e5b57ba93ca1b1e948909d5816229024010fe2b4
Matt B <mattb.prg@gmail.com>
2022-07-25 08:58:45 +1000

    style posts

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -24,9 +24,16 @@ export default function Post({ post }) {
             <Head>
                 <title>{post.title}</title>
             </Head>
-            <h2>{post.title}</h2>
-            <div className="prose" dangerouslySetInnerHTML={{ __html: post.htmlContent }}>
-            </div>
-        </Layout>
+            <article className="container mx-auto flex flex-col space-y-6 max-w-[65ch]">
+                <div className="flex flex-col space-y-2 text-center">
+                    <h1 className="text-2xl font-bold">{post.title}</h1>
+                    <span className="italic text-lg">{post.description}</span>
+                </div>
+                <div className="flex justify-center">
+                    <div className="prose" dangerouslySetInnerHTML={{ __html: post.htmlContent }}>
+                    </div>
+                </div>
+            </article>
+        </Layout >
     )
 }
