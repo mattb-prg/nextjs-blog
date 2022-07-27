4f63f649c6bd08d18386f9266a9f77f156fce911
mattb-prg <mattb.prg@gmail.com>
2022-07-27 11:49:19 +1000

    rotate carousel automatically

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -6,6 +6,7 @@ interface ICarouselProps {
     className?: string
 }
 export const Carousel: FC<ICarouselProps> = (props) => {
+    const slideCount = props.children.length
     const [activeSlide, setActiveSlide] = useState(0);
     const [containerWidth, setContainerWidth] = useState<number | undefined>();
     const [isResizing, setIsResizing] = useState(false);
@@ -34,6 +35,17 @@ export const Carousel: FC<ICarouselProps> = (props) => {
             window.removeEventListener('resize', onWindowResize)
         }
     }, [])
+
+    useEffect(() => {
+        const int = window.setTimeout(() => {
+            const nextSlide = activeSlide === (slideCount - 1) ? 0 : activeSlide + 1
+            setActiveSlide(nextSlide)
+        }, 5000)
+
+        return () => {
+            window.clearTimeout(int)
+        }
+    }, [activeSlide])
 
     return (
         <div ref={containerRef} className="flex flex-col items-center overflow-hidden space-y-4">
