d38141134d5d8ce75b5ab39684527477320aa572
mattb-prg <mattb.prg@gmail.com>
2022-07-28 15:33:27 +1000

    remove api handler

--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiHandler, NextApiRequest } from "../../node_modules/next/dist/shared/lib/utils"
-
-const handler: NextApiHandler = (req, res) => {
-    res.status(200).json({ text: 'Hello!' })
-}
-
-export default handler
